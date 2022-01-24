d5f35c9d615483226f6cdc2b372b943892dc5aaa
Ashton Rhodes <16RhodeA@fulford.york.sch.uk>
2022-01-24 13:09:49 +0000

    added help message

--- String Comparer/Form1.Designer.cs
+++ String Comparer/Form1.Designer.cs
@@ -40,16 +40,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(656, 392);
+            this.button1.Location = new System.Drawing.Point(445, 306);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 87);
             this.button1.TabIndex = 0;
             this.button1.Text = "Help";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(151, 378);
+            this.button2.Location = new System.Drawing.Point(29, 299);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(141, 101);
             this.button2.TabIndex = 0;
@@ -59,7 +60,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(189, 136);
+            this.label1.Location = new System.Drawing.Point(65, 102);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 1;
@@ -68,7 +69,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(189, 201);
+            this.label2.Location = new System.Drawing.Point(65, 167);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 1;
@@ -76,7 +77,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(272, 129);
+            this.textBox1.Location = new System.Drawing.Point(148, 95);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(451, 20);
             this.textBox1.TabIndex = 2;
@@ -84,7 +85,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(272, 198);
+            this.textBox2.Location = new System.Drawing.Point(148, 164);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(451, 20);
             this.textBox2.TabIndex = 2;
@@ -92,7 +93,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(401, 48);
+            this.label3.Location = new System.Drawing.Point(277, 14);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(82, 13);
             this.label3.TabIndex = 3;
@@ -101,7 +102,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(228, 279);
+            this.label4.Location = new System.Drawing.Point(104, 245);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(469, 13);
             this.label4.TabIndex = 4;
@@ -112,7 +113,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(857, 582);
+            this.ClientSize = new System.Drawing.Size(655, 427);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
--- String Comparer/Form1.cs
+++ String Comparer/Form1.cs
@@ -26,5 +26,11 @@ namespace String_Comparer
             }
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("enter 2 values into box 1 and box 2 then click calculate");
+            Console.WriteLine("this code is meant to recieve two strings, x and y, then it will print the longest string a of letters such that there is a permutation of a that is a subsequence of x and there is a permutation of a that is a subsequence of y");
+        }
     }
 }
